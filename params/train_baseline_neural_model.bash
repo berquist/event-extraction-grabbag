82f5e7a08dd8aa3766de2dd69502033e248cb9cb
Eric Berquist <berquist@isi.edu>
2019-09-10 11:01:53 -0400

    Update neural model training params

--- params/train_baseline_neural_model.bash
+++ params/train_baseline_neural_model.bash
@@ -4,5 +4,5 @@ set -euo pipefail
 
 SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-python -m gaia_event_extraction.model_trainers.train_baseline_neural_model \
+ipython -m gaia_event_extraction.model_trainers.train_baseline_neural_model \
        "${SCRIPTDIR}"/train_baseline_neural_model.yaml
---
+++ params/train_baseline_neural_model_batched.bash
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+ipython -m gaia_event_extraction.model_trainers.train_baseline_neural_model_batched \
+       "${SCRIPTDIR}"/train_baseline_neural_model_batched.yaml
