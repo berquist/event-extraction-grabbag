0f5aafbe2f3cff7c7a527cfbfcf49d095b9021d8
Eric Berquist <berquist@isi.edu>
2019-09-12 12:05:52 -0400

    Add scripts and params for ingesting ACE train/test/dev documents

---
+++ params/run_ace_ingester_train.bash
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+python -m gaia_event_extraction.drivers.run_ace_or_ere_ingester \
+       "${SCRIPTDIR}"/run_ace_ingester_train.yaml
