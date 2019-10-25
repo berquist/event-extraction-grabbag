e718faadf9be4518a0124458ed0024403b81b005
Eric Berquist <berquist@isi.edu>
2019-10-25 12:28:19 -0400

    mv run_ace_or_ere_ingester{,_for_gold_annotations}.py

--- params/run_ace_ingester_test.bash
+++ params/run_ace_ingester_test.bash
@@ -4,5 +4,5 @@ set -euo pipefail
 
 SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-python -m gaia_event_extraction.drivers.run_ace_or_ere_ingester \
+python -m gaia_event_extraction.drivers.run_ace_or_ere_ingester_for_gold_annotations \
        "${SCRIPTDIR}"/run_ace_ingester_test.yaml
