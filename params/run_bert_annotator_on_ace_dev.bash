972e448f58dc9418aa42f72ac2498c31f8c1adac
Eric Berquist <berquist@isi.edu>
2019-10-25 12:24:48 -0400

    split running BERT annotator onto train/dev/test

---
+++ params/run_bert_annotator_on_ace_dev.bash
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+python -m gaia_event_extraction.drivers.run_bert_annotator_on_ace \
+       "${SCRIPTDIR}"/../repos/gaia-event-extraction/sample_params/run_bert_annotator_on_ace_dev.params
