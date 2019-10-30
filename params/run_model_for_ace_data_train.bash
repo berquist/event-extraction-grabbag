70e47a7d5db532366c5875a2219bb73dfb45f4c9
Eric Berquist <berquist@isi.edu>
2019-10-30 16:32:53 -0400

    params for running ACE-trained predictors

---
+++ params/run_model_for_ace_data_train.bash
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+ipython -m gaia_event_extraction.drivers.run_model_for_ace_data \
+       "${SCRIPTDIR}"/run_model_for_ace_data_train.yaml
